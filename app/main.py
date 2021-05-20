b9674fe55b0e6d4ca133431c2e81088e8f78e7e3
Jakub Zacharczuk <zacharczuk.jakub2k@gmail.com>
2021-05-20 21:33:08 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,10 +5,3 @@ from .views import router as api_router
 app = FastAPI()
 
 app.include_router(api_router)
-
-
-@app.get("/")
-def hi():
-    print("probuje geta na /")
-    today = date.today()
-    return {"Hello, today is": str(today.year)}
