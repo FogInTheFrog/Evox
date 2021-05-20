6d321c086b1fb3c453f4381ffbbf3539125adc60
Jakub Zacharczuk <zacharczuk.jakub2k@gmail.com>
2021-05-20 21:06:46 +0200

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,17 +1,19 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://joelitpxahiqgt:ffe6958df7eb1e9f250d1e557ad63e829c4114c7c90bad2ad2adefffc8800cc4@ec2-52-213-119-221.eu-west-1.compute.amazonaws.com/dfmu8mbnjpm39u"
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-# Dependency
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
