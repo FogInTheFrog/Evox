f003850f9a34afc54f43863edf3dee21e25efcc5
Jakub Zacharczuk <zacharczuk.jakub2k@gmail.com>
2021-05-21 14:31:01 +0200

    Update login.py

--- app/login.py
+++ app/login.py
@@ -46,12 +46,14 @@ def verify_password(plain_password, hashed_password):
 
 def authenticate_user(db: Session, username: str, password: str):
     user = get_user_by_username(db, username)
+    print(user.username, user.hashed_password, user.full_name)
     if not user:
         return False
-    user_db = User.from_orm(user)
-    if not verify_password(password, user_db.hashed_password):
+    # user_db = User.from_orm(user)
+    # print(user_db.username, user_db.hashed_password,)
+    if not verify_password(password, user.hashed_password):
         return False
-    return user_db
+    return user
 
 
 def get_password_hash(password):
